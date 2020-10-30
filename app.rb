8b6efa65bb5306268c84fc08469779e6d3f45adb
cassandrawhitty <cassandraallexwhitty@gmail.com>
2020-10-30 07:04:57 -0600

    commit tree 6b12f1c53442b102fce0eafe9fbc7b413ae99485

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Company
     attr_accessor :name, :size
 
@@ -14,3 +16,57 @@ companies = [
 ]
 
 ### Your code below this
+def all_companies(companies)
+  companies.map do |company|
+    "#{company.name} - #{company.size}"
+  end
+end
+
+ # pp all_companies(companies)
+
+def combine_sizes(companies)
+  companies.reduce(0) do |sum, company|
+    sum + company.size
+  end
+end
+
+# pp combine_sizes(companies)
+
+def over_one_hundred(companies)
+  companies.select do |company|
+    company.size > 100
+  end
+end
+
+# pp over_one_hundred(companies)
+
+def find_beta(companies)
+    companies.find do |company|
+        company.name == "Beta"
+    end
+end
+
+# pp find_beta(companies)
+
+def largest_company(companies)
+  companies.reduce do |largest_company, current_company|
+    if current_company.size > largest_company.size 
+        largest_company = current_company
+    end
+  end
+end
+
+# pp largest_company(companies)
+
+def sort_companies(companies)
+  companies.reduce do |largest_company, current_company|
+    binding.pry
+    if current_company.size > largest_company.size
+      current_company.pop
+      current_company.unshift
+    end
+  end
+end 
+
+pp sort_companies(companies)
+
